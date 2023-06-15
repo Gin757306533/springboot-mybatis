b274e958b75a969357e0ce17716eec94637bc1e8
jiexu <jiexu@example.com>
2023-06-15 17:19:24 +0800

    use @Select in AccountMapper

--- src/main/java/com/example/springbootmybatis/mapper/AccountMapper.java
+++ src/main/java/com/example/springbootmybatis/mapper/AccountMapper.java
@@ -1,10 +1,12 @@
 package com.example.springbootmybatis.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 //@Mapper
 public interface AccountMapper {
+    @Select("select * from account;")
     List<Account> findAll();
 }
